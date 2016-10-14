bbeb6bf92d3858a83a89289c8e69a07fb71944b5
Colin Mackenzie <nilocski@yahoo.com>
2016-10-14 20:52:10 +0200

    Initial Commit

---
+++ plist2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  plist2
+//
+//  Created by Colin Mackenzie on 14/10/2016.
+//  Copyright © 2016 cdmackenzie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
